0e2f265c644176cf3fbac97c2f208b3a63fe66d4
Boris Joffe <boris@joff3.com>
2016-08-19 11:31:34 -0700

    use json-loader and react to load / display jobs

--- client/index.js
+++ client/index.js
@@ -1,17 +1,48 @@
 'use strict'
 
-// import _ from 'lodash'
-// import React from 'react'
-const ReactDOM = require('react-dom')
-const h = require('react-hyperscript')
-
 if (module.hot) {
 	module.hot.accept()
 }
 
+// const _ = require('lodash')
+// const React = require('react')
+const ReactDOM = require('react-dom')
+const h = require('react-hyperscript')
+
+const APP = {
+	jobs: {
+		// TODO: get from server
+		upwork: require('../data/upwork.db.json'),
+		// upwork: [],
+	},
+}
+
+const unsafeHtmlProp = (htmlStr) => {
+	return { dangerouslySetInnerHTML: {__html: htmlStr}}
+}
+
+const Jobs = () => {
+	var upworkJobs = APP.jobs.upwork.items
+
+	return h('ul', [
+		upworkJobs.map((job) => h(UpworkJob, {job})),
+	])
+}
+
+const UpworkJob = (props) => {
+	var job = props.job
+
+	return h('li', [
+		h('div', {title: Object.keys(job)}, job.title),
+		h('div', job.date),
+		h('div', unsafeHtmlProp(job.description)),
+	])
+}
+
 const App = (/* props */) => {
 	return h('div', [
-		h('span', 'Hello World'),
+		h('h1', 'Jobs'),
+		h(Jobs),
 	])
 }
 
