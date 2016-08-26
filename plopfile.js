1158f66f2ac827b420759f2d098ba2659576401c
Boris Joffe <boris@joff3.com>
2016-08-26 10:01:07 -0700

    plop - better js generator

--- plopfile.js
+++ plopfile.js
@@ -8,19 +8,24 @@ module.exports = function (plop) {
 		prompts: [{
 			type: 'input',
 			name: 'filename',
-			message: 'Enter a relative filename/path (without the js extension)',
+			message: 'Enter a relative filename/path:',
 			filter: function (filename) {
 				console.log('entered:', filename)
 
-				if (/\//.test(filename))
-					return filename
-				else
+				// create folder with index.js
+				// e.g. 'server/' -> 'server/index.js'
+				if (filename.endsWith('/'))
 					return filename + '/index'
+
+				if (!filename.endsWith('.js'))
+					filename += '.js'
+
+				return filename
 			},
 		}],
 		actions: [{
 			type: 'add',
-			path: '{{filename}}.js',
+			path: '{{filename}}',
 			templateFile: 'plop/js.templ',
 		}],
 	})
